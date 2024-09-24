b32cdf3d1f22e0e53d240ce5456526ac3b26fe96
luca <luca.rigamonti@itsrizzoli.it>
2024-09-24 17:30:00 +0200

    Costruttori

--- src/main/java/com/gestionePersonale/demo/Curatore.java
+++ src/main/java/com/gestionePersonale/demo/Curatore.java
@@ -1,4 +1,7 @@
 package com.gestionePersonale.demo;
 
 public class Curatore extends Personale {
+    public Curatore(int id, String nome, String cognome) {
+        super(id, nome, cognome);
+    }
 }
--- src/main/java/com/gestionePersonale/demo/Guida.java
+++ src/main/java/com/gestionePersonale/demo/Guida.java
@@ -1,4 +1,7 @@
 package com.gestionePersonale.demo;
 
 public class Guida extends Personale {
+    public Guida(int id, String nome, String cognome) {
+        super(id, nome, cognome);
+    }
 }
