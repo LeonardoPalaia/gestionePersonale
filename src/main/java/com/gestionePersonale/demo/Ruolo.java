861f32e6d5d9c01bc32530cc4d8b8e791f1b8fbd
emanueleblini <emanuele.blini04@gmail.com>
2024-09-24 17:20:44 +0200

    Classi Ruolo e Sezione

---
+++ src/main/java/com/gestionePersonale/demo/Ruolo.java
@@ -0,0 +1,11 @@
+package com.gestionePersonale.demo;
+
+public class Ruolo {
+    private int id;
+    private String descrizione;
+
+    public Ruolo(int id, String descrizione) {
+        this.id = id;
+        this.descrizione = descrizione;
+    }
+}
