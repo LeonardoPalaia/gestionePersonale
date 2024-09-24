ef8d9dbc73c72c01001a186bc2b0c5472e0a9183
LeonardoPalaia <leonardo.palaia@itsrizzoli.it>
2024-09-24 16:32:44 +0200

    First

---
+++ src/main/java/com/gestionePersonale/demo/GestionePersonaleMuseoApplication.java
@@ -0,0 +1,13 @@
+package com.gestionePersonale.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionePersonaleMuseoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionePersonaleMuseoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestionePersonale/demo/GestionePersonaleMuseoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestionePersonale.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionePersonaleMuseoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
