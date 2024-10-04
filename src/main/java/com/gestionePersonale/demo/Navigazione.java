7799654335cb62f1042a1e2c8b5b0cd84bfc4a95
luca <luca.rigamonti@itsrizzoli.it>
2024-10-04 14:11:55 +0200

    Create Mapping

--- src/main/java/com/gestionePersonale/demo/Navigazione.java
+++ src/main/java/com/gestionePersonale/demo/Navigazione.java
@@ -1,8 +1,28 @@
 package com.gestionePersonale.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class Navigazione {
+    @RequestMapping(value = "/areaUtente")
+    public String areaUtente() {
+        return "areaUtente";
+    }
+
+    @RequestMapping(value = "/areaAmministratore")
+    public String areaAmministratore() {
+        return "areaAmministratore";
+    }
+    @RequestMapping(value = "/login")
+    public String login() {
+        return "login";
+    }
+
+    @RequestMapping(value = "/registrazione")
+    public String registrazione() {
+        return "registrazione";
+    }
 
 }
