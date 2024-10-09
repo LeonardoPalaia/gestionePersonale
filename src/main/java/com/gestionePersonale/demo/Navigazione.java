a8ab8cd23807de241b298883ff80e1e66cdebb9d
emanueleblini <emanuele.blini04@gmail.com>
2024-10-09 12:03:33 +0200

    aggiornamento navigazione

--- src/main/java/com/gestionePersonale/demo/Navigazione.java
+++ src/main/java/com/gestionePersonale/demo/Navigazione.java
@@ -2,37 +2,34 @@ package com.gestionePersonale.demo;
 
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.ArrayList;
 
 @Controller
+@SessionAttributes("utenteLoggato")
 public class Navigazione {
-    ArrayList<Personale> utentiLoggati = new ArrayList<>();
 
-    @RequestMapping(value = "/areaUtente")
+    @GetMapping(value = "/areaUtente")
     public String areaUtente() {
         return "areaUtente";
     }
-
-    @RequestMapping(value = "/areaAmministratore")
+    @GetMapping(value = "/areaAmministratore")
     public String areaAmministratore() {
         return "areaAmministratore";
     }
-    @RequestMapping(value = "/login")
+    @GetMapping(value = "/login")
     public String login() {
         return "login";
     }
-
     @PostMapping("login")
-    public String login(@Valid Personale personale, BindingResult bindingResult) {
+    public String login(@Valid Personale personale, BindingResult bindingResult, Model model) {
         if(bindingResult.hasErrors())
             return "login";
 
-        utentiLoggati.add(personale);
+        model.addAttribute("loggedInUser", personale);
         return "redirect:/areaUtente";
     }
-
 }
