7906ccfe0837c160779b2b871661c9994756885a
luca <luca.rigamonti@itsrizzoli.it>
2024-10-09 14:22:04 +0200

    getter setter

--- src/main/java/com/gestionePersonale/demo/Sezione.java
+++ src/main/java/com/gestionePersonale/demo/Sezione.java
@@ -8,4 +8,20 @@ public class Sezione {
         this.id = id;
         this.descrizione = descrizione;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
 }
