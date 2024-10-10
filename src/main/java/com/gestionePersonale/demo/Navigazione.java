2ff4bac6094312b7b8510f1695253c2d148c8f29
emanueleblini <emanuele.blini04@gmail.com>
2024-10-10 21:48:15 +0200

    gestione login

--- src/main/java/com/gestionePersonale/demo/Navigazione.java
+++ src/main/java/com/gestionePersonale/demo/Navigazione.java
@@ -2,31 +2,40 @@ package com.gestionePersonale.demo;
 
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class Navigazione {
-    private boolean isLoggedIn = false;
+    private Credenziali utenteLoggato;
+
     @GetMapping("/index")
-    public String index() {
-        if(isLoggedIn) {
-            return "index";
+    public String home(Model model) {
+        if (utenteLoggato == null) {
+            return "redirect:/login";
         }
-        else return "login";
+        model.addAttribute("utenteLoggato", utenteLoggato);
+        return "index";
     }
+
     @GetMapping("/login")
-    public String login() {
+    public String loginForm(Model model) {
+        if (utenteLoggato != null) {
+            return "redirect:/index";
+        }
+        model.addAttribute("credenziali", new Credenziali());
         return "login";
     }
 
     @PostMapping("/login")
-    public String login(@Valid Credenziali credenziali, BindingResult bindingResult) {
+    public String login(@Valid @ModelAttribute("credenziali") Credenziali credenziali, BindingResult bindingResult, Model model) {
         if (bindingResult.hasErrors()) {
             return "login";
         }
-        // Validazione con il DB
-        isLoggedIn = true;
+        utenteLoggato = credenziali;
         return "redirect:/index";
     }
 }
