2ff4c2152622578eda035b2683252e3e5db15b1b
emanueleblini <emanuele.blini04@gmail.com>
2024-10-16 14:57:44 +0200

    Entity Ruolo

--- src/main/java/com/gestionePersonale/demo/Ruolo.java
+++ src/main/java/com/gestionePersonale/demo/Ruolo.java
@@ -1,12 +1,24 @@
 package com.gestionePersonale.demo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Ruolo {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
     private int id;
     private String descrizione;
 
     public Ruolo(int id, String descrizione) {
         this.id = id;
         this.descrizione = descrizione;
+    }
+
+    public Ruolo() {
+
     }
 
     public int getId() {
