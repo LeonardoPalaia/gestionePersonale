25eb1a30b189a1297ed9e451912903fe6666fd40
emanueleblini <emanuele.blini04@gmail.com>
2024-10-16 15:00:28 +0200

    Entity Ruolo

--- src/main/java/com/gestionePersonale/demo/Ruolo.java
+++ src/main/java/com/gestionePersonale/demo/Ruolo.java
@@ -12,8 +12,7 @@ public class Ruolo {
     private int id;
     private String descrizione;
 
-    public Ruolo(int id, String descrizione) {
-        this.id = id;
+    public Ruolo(String descrizione) {
         this.descrizione = descrizione;
     }
 
