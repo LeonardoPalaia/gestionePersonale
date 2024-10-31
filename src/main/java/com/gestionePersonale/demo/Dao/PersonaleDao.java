3b13f8378077bdfba34c57ebd8c326f26ef42a76
LeonardoPalaia <leonardo.palaia@itsrizzoli.it>
2024-10-31 09:21:09 +0100

    fix id in PersonaleDao.java.java

--- src/main/java/com/gestionePersonale/demo/Dao/PersonaleDao.java
+++ src/main/java/com/gestionePersonale/demo/Dao/PersonaleDao.java
@@ -4,6 +4,6 @@ import com.gestionePersonale.demo.model.Personale;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PersonaleDao extends JpaRepository<Personale, Integer> {
-    // Qui puoi aggiungere metodi di query personalizzati se necessario
-    Personale findByEmail(String email); // Un esempio di query personalizzata
+
+    Personale findByEmail(String email);
 }
