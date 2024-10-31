4570b135ed1b882b583b77cec0cc69ea678fde74
emanueleblini <emanuele.blini04@gmail.com>
2024-10-31 10:55:11 +0100

    Aggiornamento area_amministratore.html

--- src/main/java/com/gestionePersonale/demo/controller/Navigazione.java
+++ src/main/java/com/gestionePersonale/demo/controller/Navigazione.java
@@ -1,32 +1,23 @@
 package com.gestionePersonale.demo.controller;
 
-import com.gestionePersonale.demo.Dao.OrarioLavoroDao;
 import com.gestionePersonale.demo.Dao.PersonaleDao;
 import com.gestionePersonale.demo.Dao.RuoloDao;
 import com.gestionePersonale.demo.model.Credenziali;
-import com.gestionePersonale.demo.model.OrarioLavoro;
 import com.gestionePersonale.demo.model.Personale;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.List;
 
 @Controller
 public class Navigazione {
 
     private final PersonaleDao personaleDao;
     private final RuoloDao ruoloDao;
-
-    @Autowired
-    private OrarioLavoroDao orarioLavoroDao;
 
     public Navigazione(PersonaleDao personaleDao, RuoloDao ruoloDao) {
         this.personaleDao = personaleDao;
@@ -116,24 +107,15 @@ public class Navigazione {
             return "redirect:/area_utente";
         }
         model.addAttribute("personale", new Personale());
-        model.addAttribute("ruoli", ruoloDao.findAll());
         return "area_amministratore";
     }
 
     @PostMapping("/areaAmministratore")
     public String processaFormAmministratore(@Valid @ModelAttribute("personale") Personale personale, BindingResult result, Model model) {
         if (result.hasErrors()) {
-            model.addAttribute("ruoli", ruoloDao.findAll());
             return "area_amministratore";
         }
-        personaleDao.save(personale);
-        return "redirect:/successo";
-    }
 
-    @GetMapping("/area_utente")
-    public String getOrariLavorativi(Model model, @RequestParam Long personaleId) {
-        List<OrarioLavoro> orari = orarioLavoroDao.findByPersonaleId(personaleId);
-        model.addAttribute("orari", orari);
-        return "area_utente";
+        return "redirect:/successo";
     }
 }
